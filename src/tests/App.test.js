758a4041ee12ac1119486f25e10029e58e7d2b07
Carlos Eduardo Fernandes Martins Salomao <kaduzeiro2013@gmail.com>
2022-05-09 14:46:20 -0300

    Primeiro Commit

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../components/renderWithRouter';
+import App from '../App';
+
+describe('Requisito 1', () => {
+  it('deve renderizar o componente App, testa-lo se vai para o "/",'
++ 'e se aparece oi na tela\'', () => {
+    const { history } = renderWithRouter(<App />);
+    const { pathname } = history.location;
+
+    const homeTitle = screen.getByRole('heading', {
+      name: 'Oi',
+    });
+  });
+});
